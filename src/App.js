8c608a614bcacdbf8cffc58da24689c72b51d339
Shabalin Slava <svaznoy0309@gmail.com>
2020-08-15 19:24:40 +0800

    added transactions methods

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import  Total  from './components/total/Total';
-import  History  from './components/history/History';
-import  Operation  from './components/operation/Operation';
+import React, { Component } from 'react';
+import Total from './components/total/Total';
+import History from './components/history/History';
+import Operation from './components/operation/Operation';
 
-function App() {
-  return (
-    <div>
-      <header>
-        <h1>Кошелек</h1>
-        <h2>Калькулятор расходов</h2>
-      </header>
 
-      <main>
-        <div className="container">
-          <Total />
-          <History />
-          <Operation />
-        </div>
-      </main>
+class App extends Component {
+  state = {
+    transactions: [],
+    description: '',
+    amount: '',
+  }
 
-    </div>
-  );
+  addTransaction = (add) => {
+    const transactions = [...this.state.transactions,
+    {
+      id: `cmr${(+new Date).toString(16)}`,
+      description: this.state.description,
+      amount: this.state.amount,
+      add
+    }
+  ]
+    this.setState({transactions,
+    description: '',
+    amount: '',
+    })
+  }
+
+  addAmount = (e) => {
+    this.setState({amount: e.target.value})
+  }
+
+  addDescription = (e) => {
+    this.setState({description: e.target.value})
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Кошелек</h1>
+          <h2>Калькулятор расходов</h2>
+        </header>
+
+        <main>
+          <div className="container">
+            <Total />
+            <History transactions={this.state.transactions} />
+            <Operation 
+            addTransaction={this.addTransaction}
+            addAmount={this.addAmount}
+            addDescription={this.addDescription}
+            description={this.state.description}
+            amount={this.state.amount}
+            />
+          </div>
+        </main>
+
+      </div>
+    )
+  }
 }
 
 export default App;
